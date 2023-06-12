f2896bc7339bab61bc56f19ac3f07a3fb32d8041
Isadora Caputo <isadora.caputo@gmail.com>
2023-06-12 14:45:02 +0200

    Define front-end routes and pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,30 @@
 import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" index element={<Home />} />
+        <Route path="/hookQuiz" element={<HookQuiz />} />
+        <Route path="/hookQuiz" element={<HookQuizFeedback />} />
+        <Route path="/lectures" element={<Lectures />} />
+        <Route path="/lectures/artists" element={<ArtistsList />} />
+        <Route path="/lectures/artists/quiz" element={<ArtistsListQuiz />} />
+        <Route
+          path="/lectures/artists/quizFeedback"
+          element={<ArtistsListQuizFeedback />}
+        />
+        <Route path="/lectures/artists/:id" element={<Artist />} />
+        <Route path="/lectures/artists/:id/quiz" element={<ArtistQuiz />} />
+        <Route
+          path="/lectures/artists/:id/quizFeedback"
+          element={<ArtistQuizFeedback />}
+        />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
