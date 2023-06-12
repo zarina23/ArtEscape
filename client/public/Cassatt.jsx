752511b7e79f6c01ea9169fd726c7cfbf8c83be0
roxgiba <rox.giba@gmail.com>
2023-06-12 21:01:10 +0200

    questions added

--- client/public/Cassatt.jsx
+++ client/public/Cassatt.jsx
@@ -1,28 +1,33 @@
 const cassatt1 = {
   title: Question1,
-  questionText: "",
-  questionImage: url,
-  answer: "",
-  option1: "",
-  option2: "",
-  option3: "",
+  questionText: "What is the name of this paiting?",
+  questionImage:
+    "https://uploads6.wikiart.org/images/mary-cassatt/the-flirtation-a-balcony-in-seville-1872.jpg!Large.jpg",
+  answer: "The Flirtation A Balcony in Seville",
+  option1: "Los chismosos",
+  option2: "The man with the hat",
+  option3: "Summer days",
 }(edited);
 
 const cassatt2 = {
   title: Question2,
-  questionText: "",
-  questionImage: url,
-  answer: url,
-  option1: url,
-  option2: url,
-  option3: url,
+  questionText:
+    "Which of the following paitings is 'At the Theater' by Cassatt?",
+  answer:
+    "https://uploads3.wikiart.org/images/mary-cassatt/at-the-theater-1879.jpg!Large.jpg",
+  option1:
+    "https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pinterest.com%2Fcarpelibrum%2Fart-at-the-theater%2F&psig=AOvVaw128kvd8BJu640bl2Am8unD&ust=1686681714763000&source=images&cd=vfe&ved=2ahUKEwioj_eUsb7_AhVUQ8AKHTCpBWYQjRx6BAgAEAw",
+  option2:
+    "https://www.washingtonpost.com/rf/image_1484w/2010-2019/WashingtonPost/2014/08/19/Style/Images/sundayimage001.jpg?t=20170517",
+  option3:
+    "https://www.artstor.org/wp-content/uploads/2019/12/CARNEGIE_4020004-490x600.jpg",
 };
 
 const cassatt3 = {
   title: Question3,
-  questionText: "",
-  answer: "",
-  option1: "",
-  option2: "",
-  option3: "",
+  questionText: "Which art movement best describes Cassatt's work?",
+  answer: "Impressionism",
+  option1: "Baroque",
+  option2: "Art Nouveau",
+  option3: "Surrealism",
 };
--- client/public/Klint.jsx
+++ client/public/Klint.jsx
@@ -1,28 +1,34 @@
 const klint1 = {
   title: Question1,
-  questionText: "",
-  questionImage: url,
-  answer: "",
-  option1: "",
-  option2: "",
-  option3: "",
+  questionText: "What is the name of this paiting?",
+  questionImage:
+    "https://uploads6.wikiart.org/images/hilma-af-klint/the-ten-largest-1907.jpg",
+  answer: "The Ten Largest.",
+  option1: "Circles.",
+  option2: "White circles.",
+  option3: "Blue and yellow circles.",
 }(edited);
 
 const klint2 = {
   title: Question2,
-  questionText: "",
-  questionImage: url,
-  answer: url,
-  option1: url,
-  option2: url,
-  option3: url,
+  questionText:
+    "Which one of this paitings is 'What a Human Being Is' by af Klimt?",
+  answer:
+    "https://uploads3.wikiart.org/images/hilma-af-klint/what-a-human-being-is-1910.jpg",
+  option1:
+    "https://studyfinds.org/wp-content/uploads/2020/12/AdobeStock_267410313-604x385.jpeg",
+  option2:
+    "https://i.etsystatic.com/36644323/r/il/4db5c9/4296968593/il_794xN.4296968593_5iyf.jpg",
+  option3:
+    "https://i.etsystatic.com/33706763/r/il/530aee/3828553553/il_794xN.3828553553_fu9o.jpg",
 };
 
 const klint3 = {
   title: Question3,
-  questionText: "",
-  answer: "",
-  option1: "",
-  option2: "",
-  option3: "",
+  questionText: "What kind of work characterizes af Klint and 'The Five'?",
+  answer:
+    "Inventive geometric visual language capable of conceptualising invisible forces both of the inner and outer worlds.",
+  option1: "A chain of fast food restaurants.",
+  option2: "A talk show on Fox News Channel.",
+  option3: "A mystery thriller miniseries",
 };
