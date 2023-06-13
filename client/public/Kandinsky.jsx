d5ad7c437a6faf3d4452555f9474d944eb3c90ca
Zarina Urazgaleyeva <urazgaleyeva.zarina@gmail.com>
2023-06-13 12:58:47 +0200

    Created questions for Picasso, Dali, and Kandinsky

---
+++ client/public/Kandinsky.jsx
@@ -0,0 +1,32 @@
+const kandinsky1 = {
+  title: "Question 1",
+  image:
+    "https://uploads7.wikiart.org/images/wassily-kandinsky/blue-mountain-1908.jpg!Large.jpg",
+  questionText: "What is the name of this painting?",
+  answer: "Blue mountain",
+  option1: "A mountain",
+  option2: "Horses",
+  option2: "Park of St. Cloud with horseman",
+};
+
+const kandinsky2 = {
+  title: "Question 2",
+  questionText: "Which of this paintings is work of Wassily Kandinsky?",
+  answer:
+    "https://uploads0.wikiart.org/images/wassily-kandinsky/several-circles-1926.jpg!Large.jpg", //Several Circles
+  option1:
+    "https://uploads5.wikiart.org/images/hilma-af-klint/atom-series-no-8-atom-on-the-ether-plane-is-in-constant-change-between-rest-and-activity-at-the-1917.jpg",
+  option2:
+    "https://uploads4.wikiart.org/images/tamara-de-lempicka/the-model-1925.jpg!Large.jpg",
+  option3:
+    "https://uploads5.wikiart.org/images/mary-cassatt/mary-ellison-embroidering-1877.jpg!Large.jpg",
+};
+
+const kandinsky3 = {
+  title: "Question 3",
+  questionText: "Where is Wassily Kandinsky from?",
+  answer: "Russia",
+  option1: "Ukraine",
+  option2: "Poland",
+  option3: "Bulgaria",
+};
