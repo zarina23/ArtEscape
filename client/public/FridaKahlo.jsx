76ee84701eebd99c480fa0ad34756cc577e03d99
isa <isadorayara.rc@gmail.com>
2023-06-13 15:47:31 +0200

    Questions added

---
+++ client/public/FridaKahlo.jsx
@@ -0,0 +1,34 @@
+export 
+
+const FridaQuestion1 =
+{
+    title: Question1,
+    questionText: "As one of the last and most famous works of Frida Kahlo, this painting captures her fundamental values, what do you think it represents?",
+    questionImage: url, "https://uploads0.wikiart.org/images/magdalena-carmen-frieda-kahlo-y-calderón-de-rivera/marxism-will-give-health-to-the-sick-1954.jpg!Large.jpg",
+    answer: "Marxism will give health to the sick",
+    option1: "Final recovery from her illness",
+    option2: "Divine revelation",
+    option3: "A tribute to nature forces",
+    };
+
+const FridaQuestion2 =
+
+{
+   title: Question2,
+   questionText: "What painting was a tribute for Frida's father and was painted after his death?",
+   answer: url, "https://uploads8.wikiart.org/images/magdalena-carmen-frieda-kahlo-y-calderón-de-rivera/me-and-my-parrots-1941.jpg!Large.jpg",
+   option1: url, "https://uploads2.wikiart.org/images/magdalena-carmen-frieda-kahlo-y-calderón-de-rivera/thinking-about-death-1943.jpg!Large.jpg",
+   option2: url, "https://uploads6.wikiart.org/images/magdalena-carmen-frieda-kahlo-y-calderón-de-rivera/my-grandparents-my-parents-and-me-1936.jpg!Large.jpg",
+   option3: url, "https://uploads1.wikiart.org/images/magdalena-carmen-frieda-kahlo-y-calderón-de-rivera/self-portrait-with-necklace-of-thorns-1940.jpg!Large.jpg",
+    };
+
+const FridaQuestion3 =
+
+{
+    title: Question3,
+    questionText: "What was one of the most important narratives in Frida's work, which she was actively known for?",
+    answer: "Political activism and Mexican indigenous values",
+    option1: "Anatomy of the human body and philosophy",
+    option2: "Romantic relationship with Diego Rivera",
+    option3: "Nature symbols and animals",
+    }         
---
+++ client/public/TamaraDeLempicka.jsx
@@ -0,0 +1,36 @@
+export 
+
+const TamaraQuestion1 = 
+
+    {
+        title: Question1,
+        questionText: "Looking at this famous painting of Tamara de Lempicka, what do you think are the main topics in her work?",
+        questionImage: url, "https://uploads6.wikiart.org/images/tamara-de-lempicka/portrait-in-the-green-bugatti-1925.jpg!Large.jpg",
+        answer: "Women's liberation with aggressive confindence and luxury elegance",
+        option1: "Aristocratic lifestyle and luxury elegance",
+        option2: "Fashion style in the modernism of the 1920s",
+        option3: "Femenine beauty and the power of nature",
+        };
+        
+const TamaraQuestion2 = 
+
+    {
+       title: Question2,
+       questionText: "Which of these paintings represents purely the art deco style that Tamara de Lempicka was known for?",
+       answer: url, "https://uploads2.wikiart.org/images/tamara-de-lempicka/girl-with-gloves-1929.jpg!Large.jpg",
+       option1: url, "https://uploads0.wikiart.org/images/tamara-de-lempicka/redheaded-woman-reading-1922.jpg!Large.jpg",
+       option2: url, "https://uploads8.wikiart.org/images/tamara-de-lempicka/double-47.jpg!Large.jpg",
+       option3: url, "https://uploads2.wikiart.org/images/tamara-de-lempicka/portrait-of-marquis-sommi-1925.jpg!Large.jpg",
+        };
+  
+const TamaraQuestion3 =
+    
+    {
+        title: Question3,
+        questionText: "What was the predominant technique and pictorical style that Tamara de Lemppicka was acclaimed for?", 
+        answer: "Art Deco style with blended cubism and neoclassicism",
+        option1: "Cubism"
+        option2: "Neorealism",
+        option3: "Surrealism and Expressionism",
+        }         
+    
