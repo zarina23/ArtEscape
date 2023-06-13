b0540acae65b9a54879bb8e8371864ce485e99df
Isadora Caputo <isadora.caputo@gmail.com>
2023-06-13 14:15:40 +0200

    Add questions and answers for Leonardo da Vinci and Ivan Aivazovsky artist quiz

--- client/public/DaVinci.js
+++ client/public/DaVinci.js
@@ -1,31 +1,40 @@
 export const DaVinciQuestions = [
   {
     title: "Question 1",
-    questionText: "",
-    questionImage: `url()`,
-    answer: "",
-    option1: "",
-    option2: "",
-    option3: "",
+    questionText:
+      "What is the name of the painting that Leonardo Da Vinci was persuaded to paint twice for the Confraternity of the Immaculate Conception in Milan?",
+    questionImage:
+      "https://uploads2.wikiart.org/00339/images/leonardo-da-vinci/virgin-of-the-rocks-between-1483-and-1486.jpg",
+    //"https://uploads3.wikiart.org/00142/images/57726d85edc2cb3880b48ccd/leonardo-da-vinci-virgin-of-the-rocks-national-gallery-london.jpg",
+    answer: "The Virgin of The Rocks",
+    option1: "The Madonna of the Carnation",
+    option2: "The Virgin and Child with Saint Anne and Saint John the Baptist",
+    option3: "Leda",
   },
 
   {
     title: "Question 2",
-    questionText: "",
-    questionImage: `url()`,
-    answer: `url()`,
-    option1: `url()`,
-    option2: `url()`,
-    option3: `url()`,
+    questionText: "What work of art did Leonardo da Vinci NOT create?",
+    questionImage: "",
+    answer:
+      "https://uploads7.wikiart.org/images/michelangelo/the-last-judgement-1541.jpg", // "The Last Judgement"
+    option1:
+      "https://uploads1.wikiart.org/images/leonardo-da-vinci/bird-s-eye-view-of-a-landscape.jpg", // "Bird's Eye View of a Landscape"
+    option2:
+      "https://uploads4.wikiart.org/00178/images/leonardo-da-vinci/ltima-cena-da-vinci-5.jpg", // "The Last Supper"
+    option3:
+      "https://uploads1.wikiart.org/00226/images/leonardo-da-vinci/1503990074029518-568314.jpg", // "Annunciation"
   },
 
   {
     title: "Question 3",
     questionText:
-      "Italian artist and intellectual Leonardo da Vinci lived during the Renaissance period. This period begun in Italy in the 14th century and spread to the rest of Europe, ending in the 17th century. The Renaissance marked a transition from ancient to modern times and many advances in art and architecture.",
-    answer: "The Last Judgement",
-    option1: "Virgin of the Rocks",
-    option2: "The Last Supper",
-    option3: "The Birth of Venus",
+      "In his final years in France, Leonardo Da Vinci did little painting and mainly worked on his scientific studies. Which is one of his greatest interests evidenced in the pieces of art below?",
+    questionImage:
+      "https://uploads4.wikiart.org/images/leonardo-da-vinci/anatomy-of-the-neck-1515.jpg",
+    answer: "Anatomy",
+    option1: "Genetics",
+    option2: "Zoology",
+    option3: "Diagnosis",
   },
 ];
