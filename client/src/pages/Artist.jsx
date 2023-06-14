4e310edd01910705cdf1a5346a951330ea43cf68
Isadora Caputo <isadora.caputo@gmail.com>
2023-06-14 10:47:21 +0200

    Create basic layout for the artist page

--- client/src/pages/Artist.jsx
+++ client/src/pages/Artist.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+import { experimentalStyled as styled } from "@mui/material/styles";
 import PropTypes from "prop-types";
 import Paper from "@mui/material/Paper";
 import Typography from "@mui/material/Typography";
@@ -7,12 +8,22 @@ import Grid from "@mui/material/Grid";
 import Link from "@mui/material/Link";
 import Box from "@mui/material/Box";
 import { Loading } from "../components/Loading";
+import Avatar from "@mui/material/Avatar";
+import Stack from "@mui/material/Stack";
+import Tabs from "@mui/material/Tabs";
+import Tab from "@mui/material/Tab";
 
 export function Artist() {
   const [artist, setArtist] = useState({});
   const [paintings, setPaintings] = useState({});
   const { id } = useParams();
   const [loading, setLoading] = useState(false);
+  const [value, setValue] = useState(0);
+
+  const Item = styled(Box)(({ theme }) => ({
+    padding: theme.spacing(2),
+    textAlign: "left",
+  }));
 
   useEffect(() => {
     getArtistAndPaintingsDetails(id);
@@ -31,6 +42,42 @@ export function Artist() {
     } catch (err) {
       console.log(err);
     }
+  };
+
+  function TabPanel(props) {
+    const { children, value, index } = props;
+
+    return (
+      <div
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+      >
+        {value === index && (
+          <Box sx={{ p: 2 }}>
+            <Typography>{children}</Typography>
+          </Box>
+        )}
+      </div>
+    );
+  }
+
+  TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.number.isRequired,
+    value: PropTypes.number.isRequired,
+  };
+
+  function a11yProps(index) {
+    return {
+      id: `simple-tab-${index}`,
+      "aria-controls": `simple-tabpanel-${index}`,
+    };
+  }
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
   };
 
   return (
@@ -57,7 +104,7 @@ export function Artist() {
               bottom: 0,
               right: 0,
               left: 0,
-              backgroundColor: "rgba(0,0,0,.3)",
+              backgroundColor: "rgba(0,0,0,.6)",
             }}
           />
           <Grid container>
@@ -77,20 +124,72 @@ export function Artist() {
                 >
                   {artist.artistName}
                 </Typography>
-                <Typography variant="h6" color="inherit" paragraph>
+                {/* <Typography variant="h6" color="inherit" paragraph>
                   {artist.OriginalArtistName}
-                </Typography>
-                <Typography variant="h5" color="inherit" paragraph>
+                </Typography> */}
+                {/* <Typography variant="h5" color="inherit" paragraph>
                   {`${artist.birthDayAsString} - ${artist.deathDayAsString}`}
-                </Typography>
+                </Typography> */}
                 <Typography variant="h5" color="inherit" paragraph>
                   {console.log(paintings)}
                 </Typography>
               </Box>
             </Grid>
           </Grid>
+          <Stack direction="row" spacing={2} sx={{ justifyContent: "center" }}>
+            <Avatar
+              sx={{ width: 200, height: 200, marginBottom: -7 }}
+              alt={artist.artistName}
+              src={artist.image}
+            />
+          </Stack>
         </Paper>
       )}
+      <Box sx={{ width: "100%" }}>
+        <Box sx={{ borderBottom: 1, borderColor: "divider" }}>
+          <Tabs
+            value={value}
+            onChange={handleChange}
+            aria-label="basic tabs example"
+          >
+            <Tab label="BIO" {...a11yProps(0)} />
+            <Tab label="PAINTINGS" {...a11yProps(1)} />
+          </Tabs>
+        </Box>
+        <TabPanel value={value} index={0}>
+          <Grid
+            container
+            rowSpacing={0}
+            columnSpacing={{ xs: 1, sm: 2, md: 6 }}
+          >
+            <Grid item xs={6}>
+              <Item>{`ORIGINAL NAME: ${artist.OriginalArtistName} `}</Item>
+            </Grid>
+            <Grid item xs={6}>
+              <Item>{`NATIONALITY: Mexico `}</Item>
+            </Grid>
+            <Grid item xs={6}>
+              <Item>{`YEAR OF BIRTH: ${artist.birthDayAsString} `}</Item>
+            </Grid>
+            <Grid item xs={6}>
+              <Item>{`YEAR OF DEATH: ${artist.deathDayAsString} `}</Item>
+            </Grid>
+            <Grid item xs={6}>
+              <Item>{`PICTORICAL STYLE: XXXXXXXXXXXXXX `}</Item>
+            </Grid>
+            <Grid item xs={6}>
+              <Item>XXXXXX</Item>
+            </Grid>
+          </Grid>
+          <br />
+          <Paper sx={{ padding: 5, textAlign: "left" }}>
+            {artist.biography}
+          </Paper>
+        </TabPanel>
+        <TabPanel value={value} index={1}>
+          PAINTINGS
+        </TabPanel>
+      </Box>
     </>
   );
 }
