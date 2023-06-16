6d2cab1ccf728bf35a67085ee454ef55d9cf2298
Isadora Caputo <isadora.caputo@gmail.com>
2023-06-16 13:58:41 +0200

    Update artist bio

--- client/src/components/ArtistQuizQ1.jsx
+++ client/src/components/ArtistQuizQ1.jsx
@@ -9,32 +9,25 @@ import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import { containerClasses } from "@mui/material";
 
-function ArtistQuizQ1({}) {
+function ArtistQuizQ1() {
   const theme = useTheme();
 
   return (
-    <Box >
-      <Paper >
+    <Box>
+      <Paper>
         <Typography>{/* question? */}</Typography>
       </Paper>
-      <Box
-       
-      >
-        <img
-        
-        />
+      <Box>
+        <img />
       </Box>
       <MobileStepper
-        
-        nextButton={
-          <Button
-            size="small">
-          </Button>
-        }
+        nextButton={<Button size="small"></Button>}
         backButton={
-          <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-          
-          </Button>
+          <Button
+            size="small"
+            // onClick={handleBack}
+            // disabled={activeStep === 0}
+          ></Button>
         }
       />
     </Box>
