1e8236e4ab58056c889c50312a25e646a4d7071c
roxgiba <rox.giba@gmail.com>
2023-06-16 15:03:56 +0200

    selecting option highlight done

--- client/src/components/ArtistQuizQ1.jsx
+++ client/src/components/ArtistQuizQ1.jsx
@@ -5,7 +5,7 @@ import "./ArtistQuizQ1.css";
 // import MobileStepper from "@mui/material/MobileStepper";
 // import Paper from "@mui/material/Paper";
 // import Typography from "@mui/material/Typography";
-// import Button from "@mui/material/Button";
+import Button from "@mui/material/Button";
 // import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 // import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 // import { containerClasses } from "@mui/material";
@@ -15,11 +15,27 @@ function ArtistQuizQ1({ quizQuestionsList }) {
 
   const [questionItemObject, setQuestionItemObject] = useState({});
   const [answerOptionsList, setAnswerOptionsList] = useState([]);
+  const [shuffledAnswerOptionList, setShuffledAnswerOptionList] = useState([]);
+  const [isUserAnswerCorrect, setIsUserAnswerCorrect] = useState(false);
+  const [userSelectedAnswer, setUserSelectedAnswer] = useState("");
 
   const filteredQuestion = quizQuestionsList.filter(
     (questionObject) =>
       questionObject.question_type === "questionImage_answersText"
   );
+
+  function shuffleArray(array) {
+    const shuffledArray = [...array];
+    for (let i = shuffledArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [shuffledArray[i], shuffledArray[j]] = [
+        shuffledArray[j],
+        shuffledArray[i],
+      ];
+    }
+    // console.log(shuffledArray);
+    setShuffledAnswerOptionList(shuffledArray);
+  }
 
   useEffect(() => {
     // console.log(filteredQuestion);
@@ -36,12 +52,26 @@ function ArtistQuizQ1({ quizQuestionsList }) {
 
   useEffect(() => {
     setAnswerOptionsList([
-      { correctAnswer: questionItemObject?.option0_text },
-      { optionAnswer1: questionItemObject?.option1_text },
-      { optionAnswer2: questionItemObject?.option2_text },
-      { optionAnswer3: questionItemObject?.option3_text },
+      { optionAnswer: questionItemObject?.option0_text },
+      { optionAnswer: questionItemObject?.option1_text },
+      { optionAnswer: questionItemObject?.option2_text },
+      { optionAnswer: questionItemObject?.option3_text },
     ]);
   }, [questionItemObject]);
+
+  useEffect(() => {
+    shuffleArray(answerOptionsList);
+  }, [answerOptionsList]);
+
+  function captureUserAnswer(event) {
+    const userAnswer = event.target.innerHTML;
+    setUserSelectedAnswer(userAnswer);
+
+    const correctAnswer = filteredQuestion[0].option0_text;
+    if (userAnswer === correctAnswer) {
+      setIsUserAnswerCorrect(true);
+    }
+  }
 
   //next
   return (
@@ -60,12 +90,25 @@ function ArtistQuizQ1({ quizQuestionsList }) {
 
       {/* Answer options */}
       <section className="answerOptionsContainer">
-        <div>
-          <p>{answerOptionsList?.[0].correctAnswer}</p>
-        </div>
-        <div></div>
-        <div></div>
-        <div></div>
+        {shuffledAnswerOptionList.map((shuffledAnswerOptionObject, i) => (
+          <div
+            onClick={(event) => captureUserAnswer(event)}
+            key={i}
+            className={
+              shuffledAnswerOptionObject?.optionAnswer === userSelectedAnswer
+                ? "selected"
+                : null
+            }
+          >
+            {shuffledAnswerOptionObject?.optionAnswer}
+          </div>
+        ))}
+      </section>
+
+      {/* action buttons */}
+      <section>
+        <Button>Check</Button>
+        <Button>Next</Button>
       </section>
     </div>
   );
