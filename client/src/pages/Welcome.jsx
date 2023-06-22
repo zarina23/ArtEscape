2a6fd706b04d2a26b8dc5fc5c1465f3624eeafa8
Zarina Urazgaleyeva <urazgaleyeva.zarina@gmail.com>
2023-06-22 12:43:46 +0200

    Styled and responsive design

--- client/src/pages/Welcome.jsx
+++ client/src/pages/Welcome.jsx
@@ -21,14 +21,14 @@ const TextContainer = styled(Box)(({ theme }) => ({
   backgroundColor: "rgba(255, 255, 255, 0.8)",
   padding: theme.spacing(3),
   borderRadius: theme.spacing(1),
-  maxWidth: 400,
+  maxWidth: 700,
   margin: theme.spacing(2),
+  marginTop: 70,
   boxShadow: "0 0 0px rgba(0, 1, 0.5, 0)",
   display: "flex",
   flexDirection: "column",
-  justifyContent: "center",
-  alignItems: "center",
-  fontFamily: "Source Serif 4",
+  justifyContent: "space-between",
+  // alignItems: "center",
 }));
 
 export function Welcome() {
@@ -48,17 +48,24 @@ export function Welcome() {
       <HeroFrame>
         {/* child one */}
         <TextContainer>
-          <h1>Welcome to ArtEscape</h1>
-          <h2>
+          <Typography variant="h2" align="center" className="welcomeH2">
+            Welcome to ArtEscape
+          </Typography>
+          <Typography variant="h5" align="center" className="welcomeH2">
             Expand your art knowledge and navigate through the most beautiful
             paintings in the art history
-          </h2>
+          </Typography>
         </TextContainer>
 
         {/* child two */}
         <div className="actionsContainer">
-          <TextContainer>
-            <Typography variant="h6" align="center" gutterBottom>
+          <TextContainer className="actionBoxes">
+            <Typography
+              className="actionText"
+              variant="h6"
+              align="center"
+              gutterBottom
+            >
               Take a first quick quiz and test your knowledge in art
             </Typography>
             <Box
@@ -75,8 +82,31 @@ export function Welcome() {
             </Box>
           </TextContainer>
 
-          <TextContainer>
-            <Typography variant="h6" align="center" gutterBottom>
+          <TextContainer className="actionBoxes">
+            <Typography
+              className="actionText"
+              variant="h6"
+              align="center"
+              gutterBottom
+            >
+              Look at the lectures
+            </Typography>
+            <Box
+              sx={{ display: "flex", justifyContent: "center", marginTop: 3 }}
+            >
+              <Button variant="contained" size="large" onClick={goToGallery}>
+                Paintings Gallery
+              </Button>
+            </Box>
+          </TextContainer>
+
+          <TextContainer className="actionBoxes">
+            <Typography
+              className="actionText"
+              variant="h6"
+              align="center"
+              gutterBottom
+            >
               Go straight to the gallery and simply enjoy art!
             </Typography>
             <Box
